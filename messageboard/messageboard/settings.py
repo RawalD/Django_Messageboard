77f6c161028c269ceaa14a61bee6feee6cc7380c
Dip Rawal <diprawal@hotmail.com>
2021-01-21 01:35:36 +0200

    Update settings.py

--- messageboard/messageboard/settings.py
+++ messageboard/messageboard/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')_j3+j#dlx8)ak8k2+=!_zccfg(*6&b_^#@e71^@afprf6*nrf'
+SECRET_KEY = 'keyhere'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
